d34490b48104c0aa090f69c2ac4375d9ee007bca
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-04-03 17:50:20 -0400

    minor changes, nothing permanent

--- src/App.svelte
+++ src/App.svelte
@@ -38,7 +38,7 @@
         // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
         // alert('Current view: ' + info.view.type);
         // change the day's background color just for fun
-        info.dayEl.style.backgroundColor = 'green';
+        info.dayEl.style.backgroundColor = 'pink';
     }});
 
     calendar.render();
