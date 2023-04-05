d25708ee258e8e9d9cc2dbb6d14ad4dea3cce8f0
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-04-05 07:14:34 -0400

    played with calendar, added day option, added form

--- src/App.svelte
+++ src/App.svelte
@@ -1,59 +1,22 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import Navbar from './lib/UI/Navbar.svelte';
-
-  // Full Calendar Plugins
-  import { Calendar } from '@fullcalendar/core';
-  import dayGridPlugin from '@fullcalendar/daygrid';
-  import timeGridPlugin from '@fullcalendar/timegrid';
-  import listPlugin from '@fullcalendar/list';
-
-  // Unused right now
-  import interactionPlugin from '@fullcalendar/interaction';
-  import multiMonthPlugin from '@fullcalendar/multimonth';
-  // import scrollGridPlugin from '@fullcalendar/scrollgrid'; // Req. premium
-  
-  onMount(() => {
-    let calendarEl: HTMLElement = document.getElementById('calendar')!;
-  
-    let calendar = new Calendar(calendarEl, {
-      // schedulerLicenseKey: '<YOUR-LICENSE-KEY-GOES-HERE>',
-      plugins: [ 
-        dayGridPlugin,
-        timeGridPlugin,
-        listPlugin,
-        interactionPlugin,
-        multiMonthPlugin
-      ],
-      initialView: 'dayGridMonth',
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,listWeek'
-      },
-      dateClick: function(info) {
-        // Do something more meaningful here
-        // let dateString: String = info.dateStr;
-        // alert(`You clicked on: ${dateString}`)
-        // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-        // alert('Current view: ' + info.view.type);
-        // change the day's background color just for fun
-        info.dayEl.style.backgroundColor = 'pink';
-    }});
-
-    calendar.render();
-  });
+  import Footer from './lib/UI/Footer.svelte';
+  import Calendar from './lib/UI/Calendar.svelte';
+  import Form from './lib/UI/Form.svelte';
 
 </script>
+
 <Navbar />
-<div id="calendar"></div>
+<main>
+  <Form />
+  <Calendar />
+</main>
+<Footer />
 
 <style>
-  #calendar {
-    margin: 0 auto;
-    padding-top: 120px;
-    height: 80vh;
-    width: 80vw;
+  main {
+    padding: 150px 0;
   }
 </style>
 
--- src/lib/UI/Calendar.svelte
+++ src/lib/UI/Calendar.svelte
@@ -1,176 +1,70 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+
+  // Full Calendar Plugins
+  import { Calendar } from '@fullcalendar/core';
+  import dayGridPlugin from '@fullcalendar/daygrid';
+  import timeGridPlugin from '@fullcalendar/timegrid';
+  import listPlugin from '@fullcalendar/list';
+
+  // Unused right now
+  import interactionPlugin from '@fullcalendar/interaction';
+  import multiMonthPlugin from '@fullcalendar/multimonth';
+  // import scrollGridPlugin from '@fullcalendar/scrollgrid'; // Req. premium
+  
+  onMount(() => {
+    const calendarEl: HTMLElement = document.getElementById('calendar')!;
+    const calendar = new Calendar(calendarEl, {
+      // schedulerLicenseKey: '<YOUR-LICENSE-KEY-GOES-HERE>',
+      plugins: [ 
+        dayGridPlugin,
+        timeGridPlugin,
+        listPlugin,
+        interactionPlugin,
+        multiMonthPlugin
+      ],
+      initialView: 'dayGridMonth',
+      headerToolbar: {
+        right: 'prev,next',
+        center: 'title',
+        left: 'dayGridMonth,timeGridWeek,timeGridDay,today'
+      },
+      events: [
+        {
+          title: 'test',
+          start: '2023-04-10T07:00:00',
+          end: '2023-04-12T16:30:00'
+        }
+      ],
+      droppable: true, // This allows things to be dropped onto the calendar
+      dateClick: function(info) {
+        // Do something more meaningful here
+        // let dateString: String = info.dateStr;
+        // alert(`You clicked on: ${dateString}`)
+        // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+        // alert('Current view: ' + info.view.type);
+
+          if(info.dayEl.style.backgroundColor !== 'rgba(197, 241, 255, 0.525)'){
+            // Open a modal with more info about event/project
+            info.dayEl.style.backgroundColor = '#c5f1ff86';
+            
+          } else {
+            info.dayEl.style.backgroundColor = 'white';
+          }
+    }});
+
+    calendar.render();
+  });
 
 </script>
-
-<main class="relative pt-40">
-  <div class="roadmap-wrapper p-2">
-  <header class="flex justify-between items-center text-center h-1/6">
-    <div class="flex">
-      <p class="rounded-l-md p-2.5 bg-emerald-800 cursor-pointer hover:bg-emerald-700 text-white">&lt;</p>
-      <p class="rounded-r-md p-2.5 bg-emerald-800 cursor-pointer hover:bg-emerald-700 text-white">&gt;</p>
-    </div>
-    <h1 class="text-4xl">Title</h1>
-    <div class="flex">
-      <p class="rounded-l-md p-2.5 bg-emerald-800 cursor-pointer hover:bg-emerald-700 text-white">Day</p>
-      <p class="p-2.5 bg-emerald-800 cursor-pointer hover:bg-emerald-700 text-white">Week</p>
-      <p class="rounded-r-md p-2.5 bg-emerald-800 cursor-pointer hover:bg-emerald-700 text-white">Month</p>
-    </div>
-  </header>
-    <div class="table-wrapper">
-      <table>
-        <thead>
-          <tr>
-            <th>Employee</th>
-            <th>Sun</th>
-            <th>Mon</th>
-            <th>Tue</th>
-            <th>Wed</th>
-            <th>Thu</th>
-            <th>Fri</th>
-            <th>Sat</th>
-            <th>Sun</th>
-            <th>Mon</th>
-            <th>Tue</th>
-            <th>Wed</th>
-            <th>Thu</th>
-            <th>Fri</th>
-            <th>Sat</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>Denzel1</th>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <th>Denzel2</th>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <th>Denzel3</th>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <th>Denzel4</th>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <th>Denzel5</th>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <th>Denzel</th>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <th>Denzel</th>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <th>Denzel</th>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-          </tr> 
-        </tbody>
-        <!-- <tfoot></tfoot> -->
-      </table>
-    </div>
-  </div>
-</main>
+<div id="calendar"></div>
 
 <style>
-  .roadmap-wrapper {
-    position: relative;
-    border: 1px solid #36363625;
-    box-shadow: 0px 2px 10px #0000001e;
-    border-top: 4px solid #10B981;
-    width: 90vw;
-    height: 70vh;
+  #calendar {
     margin: 0 auto;
-    padding-bottom: 50px;
+    padding-top: 120px;
+    height: 80vh;
+    width: 80vw;
   }
-
-  .table-wrapper {
-    overflow: scroll;
-    height: 80%;
-  }
-
-  table {
-    display: block;
-    position: relative;
-    text-align: center;
-  }
-
-  thead {
-    position: sticky;
-    top: 0;
-  }
-
-  thead th {
-    height: 80px;
-  }
-
-  tbody {
-    overflow: scroll;
-  }
-
-  tr {
-    border: 1px solid #e0e0e0;
-    padding-top: 100px;
-  }
-
-  th, td {
-    border: 1px solid #e0e0e0;
-    padding: 30px 20px;
-    white-space: nowrap;
-  }
-
 </style>
 
