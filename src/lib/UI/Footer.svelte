4faf7002d08461a039da793d4385a57032f943e4
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-04-05 07:13:49 -0400

    created very basic footer, just for visuals

---
+++ src/lib/UI/Footer.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<footer class="w-screen py-6 bg-cyan-950 text-white font-bold">
+    <p class="text-center text-2xl">Footer</p>
+</footer>
+
+<style>
+    footer {
+        z-index: 10;
+        opacity: 95%;
+    }
+</style>
